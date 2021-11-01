2579daf230f1df71551cc7bdf0988879311890ea
Gerald <hakeem1106@gmail.com>
2021-11-01 17:18:40 -0400

    pulls data

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+import csv
+
+url = "https://finance.yahoo.com/quote/AAPL/history"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html5lib")
+
+print(soup)
+
 if __name__ == "__main__":
     pass
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,2 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+import csv
+
+url = "http://www.footballlocks.com/nfl_point_spreads.shtml"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, features='html5lib')
+
+favorite = []
+underdog =[]
+spread = {}
+table_title = soup.find_all(string=["Favorite", "Underdog"])
+table_data = soup.find_all(attrs={"width":580})
+
+def find():
+
+   search=input("Which team do you want to see? ")
+   for data in table_data:
+       if search == data:
+           print("tr")
+
+find()
+
 if __name__ == "__main__":
     pass
